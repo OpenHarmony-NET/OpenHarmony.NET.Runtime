2c1208ff54c80c62661194935448c9ba8cd970fa
Bubu <54771283+CeSun@users.noreply.github.com>
2025-04-22 18:00:22 +0800

    Update build-openharmony-x64.sh

--- scripts/build-openharmony-x64.sh
+++ scripts/build-openharmony-x64.sh
@@ -5,8 +5,7 @@ docker run --rm \
     -w /runtime \
     -e ROOTFS_DIR="/crossrootfs/x64" \
     mcr.microsoft.com/dotnet-buildtools/prereqs:azurelinux-3.0-net9.0-cross-amd64-musl \
-    bash -c "ls -al \
-    ./build.sh --subset clr.aot+libs --configuration Release -arch x64 --cross \
+    bash -c "./build.sh --subset clr.aot+libs --configuration Release -arch x64 --cross \
     && mkdir -p /package/runtime.linux-musl-x64.microsoft.dotnet.ilcompiler/sdk/ \
     && mkdir -p /package/runtime.linux-musl-x64.microsoft.dotnet.ilcompiler/framework/ \
     && cp -p ./artifacts/bin/coreclr/linux.x64.Release/aotsdk/* /package/runtime.linux-musl-x64.microsoft.dotnet.ilcompiler/sdk/ \
